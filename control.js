195eb6425c7c6395df1229978cc2e13fd0b2b179
gabrielbtera <gabriel.jesus@dcomp.ufs.br>
2023-01-04 14:49:09 -0300

    ajustes site luana

--- control.js
+++ control.js
@@ -1,10 +1,16 @@
 var fale = document.getElementById("fale");
 var sobre = document.getElementById("sobre");
 
+var divFaleComigo = document.getElementById("fale-comigo");
+var divSobreMim = document.getElementById("sobre-mim");
+
 function falar() {
   sobre.style.color = "#0d2f02";
   fale.style.color = "#218103";
+  // divSobreMim.style.display = "none";
 }
+
+falar();
 
 function falarSobre() {
   fale.style.color = "#0d2f02";
